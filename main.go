6b5f0091ee5110a8dc4c0526ff4eaa90c8576794
Vetal <vlyashchenko01@gmail.com>
2023-11-09 21:04:27 +0200

    Structure

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
 )
 
+// Function for padding
+func padding(message []byte) []byte {
+
+}
+
+// Permutation function
+func permutation(state [][]uint64, outputLength int) []byte {
+
+}
+
+// SHA3 func
+func sha3(message []byte, outputLength int) []byte {
+
+}
+
 func main() {
-	fmt.Println("Test")
+	const input = "This is just a simple test."
+	h := sha256.New()
+	h.Write([]byte(input))
+
+	fmt.Printf("%x\n", h.Sum(nil))
 }

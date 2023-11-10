fe2ef30b9bb1c20d7f5b2a81c3c47efee9c4d4b9
Vetal <vlyashchenko01@gmail.com>
2023-11-10 14:29:31 +0200

    SHA-1

--- main.go
+++ main.go
@@ -5,21 +5,6 @@ import (
 	"fmt"
 )
 
-// Function for padding
-func padding(message []byte) []byte {
-
-}
-
-// Permutation function
-func permutation(state [][]uint64, outputLength int) []byte {
-
-}
-
-// SHA3 func
-func sha3(message []byte, outputLength int) []byte {
-
-}
-
 func main() {
 	const input = "This is just a simple test."
 	h := sha256.New()

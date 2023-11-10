576180a99a918088b67bc369e19ecf1b0142f6a8
Vetal <vlyashchenko01@gmail.com>
2023-11-10 14:31:30 +0200

    SHA-1

--- main.go
+++ main.go
@@ -1,14 +1,35 @@
 package main
 
-import (
-	"crypto/sha256"
-	"fmt"
-)
+import "fmt"
+
+// "fmt"
+
+var h0, h1, h2, h3, h4 uint32 = 0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0
+
+func f(t uint32, b, c, d uint32) uint32 {
+
+}
+
+func k(t uint32) uint32 {
+	switch {
+	case t < 20:
+		return 0x5A827999
+	case t < 40:
+		return 0x6ED9EBA1
+	case t < 60:
+		return 0x8F1BBCDC
+	default:
+		return 0xCA62C1D6
+	}
+}
+
+// Функция хеширования
+func sha(message []byte) [20]byte {
+
+}
 
 func main() {
-	const input = "This is just a simple test."
-	h := sha256.New()
-	h.Write([]byte(input))
+	message := "Your message here"
 
-	fmt.Printf("%x\n", h.Sum(nil))
+	fmt.Println(message)
 }

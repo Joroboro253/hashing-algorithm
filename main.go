3e7ca040a16d1453464dc34ff7a3d773216dff3a
Vetal <vlyashchenko01@gmail.com>
2023-11-12 13:31:12 +0200

    new func

--- main.go
+++ main.go
@@ -1,36 +1,33 @@
 package main
 
-import (
-	"fmt"
-)
-
 // "fmt"
-
+// 32 beat var
 var h0, h1, h2, h3, h4 uint32 = 0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0
 
 // func for each round
-func f(t uint32, b, c, d uint32) uint32 {
+func chooseFunction(i int, b, c, d uint32) uint32 {
 	switch {
-	case t < 20:
+	case i < 20:
 		// функция выбора, ch (choice)
 		return (b & c) | ((^b) & d)
-	case t < 40:
+	case i < 40:
 		return b ^ c ^ d
-	case t < 60:
-		// функйция большинства
+	case i < 60:
+		// функция большинства
 		return (b & c) | (b & d) | (c & d)
 	default:
 		return b ^ c ^ d
 	}
 }
 
-func k(t uint32) uint32 {
+// func for cost on different round of hashing
+func constantFunction(i int) uint32 {
 	switch {
-	case t < 20:
+	case i < 20:
 		return 0x5A827999
-	case t < 40:
+	case i < 40:
 		return 0x6ED9EBA1
-	case t < 60:
+	case i < 60:
 		return 0x8F1BBCDC
 	default:
 		return 0xCA62C1D6
@@ -42,8 +39,31 @@ func k(t uint32) uint32 {
 
 // }
 
-func main() {
-	message := "Your message here"
+func processBlock(a, b, c, d, e uint32, w [80]uint32) (uint32, uint32, uint32, uint32, uint32) {
+	aa, bb, cc, dd := a, b, c, d
+	ee := e
+	for i := 0; i < 80; i++ {
+		temp := leftRotate(aa, 5) + chooseFunction(i, bb, cc, dd) + ee + w[i] + constantFunction(i)
 
-	fmt.Println(message)
+		ee = dd
+		dd = cc
+		cc = leftRotate(bb, 30)
+		bb = aa
+		aa = temp
+	}
+	a = (a + aa) & 0xFFFFFFFF
+	b = (b + bb) & 0xFFFFFFFF
+	c = (c + cc) & 0xFFFFFFFF
+	d = (d + dd) & 0xFFFFFFFF
+	e = (e + ee) & 0xFFFFFFFF
+	return a, b, c, d, e
+}
+
+// Вспомогательная функция для циклического сдвига влево
+func leftRotate(value uint32, shiftBits uint32) uint32 {
+	return ((value << shiftBits) | (value >> (32 - shiftBits))) & 0xFFFFFFFF
+}
+
+func main() {
+
 }
